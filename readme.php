0825d3013931fddad2f3fae61d31ab89f3f1c414
RaaviHemaSai <hemasairaavi35@gmail.com>
2025-06-13 10:35:30 +0530

    Add files via upload

---
+++ readme.php
@@ -0,0 +1,6 @@
+After importing successfully.Add commentMore actions
+
+Open any browser and type http://localhost/bdms/home.php to open User layout.
+Open any browser and type http://localhost/BDMS/home.php to open User layout.
+Admin Panel
+Open any browser and type http://localhost/bdms/admin/login.php to open Admin Login Panel. Open any browser and type http://localhost/BDMS/admin/login.php to open Admin Login Panel.
